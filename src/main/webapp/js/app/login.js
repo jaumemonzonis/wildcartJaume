7fe65eed5e4dbaf1a849d12d460fcb3976769c99
Carlos <Carlos@DESKTOP-PPN9LHF>
2018-11-14 00:06:26 +0100

    arreglando fallos

--- src/main/webapp/js/app/login.js
+++ src/main/webapp/js/app/login.js
@@ -3,27 +3,18 @@
 moduleLogin.controller('loginController', ['$scope', '$http', '$location', 'toolService', '$routeParams',
     function ($scope, $http, $location, toolService, $routeParams) {
         $scope.validar = function () {
-            var json = {
-                id: $scope.ajaxDatoUsuario.id,
-                dni: $scope.ajaxDatoUsuario.dni,
-                nombre: $scope.ajaxDatoUsuario.nombre,
-                ape1: $scope.ajaxDatoUsuario.ape1,
-                ape2: $scope.ajaxDatoUsuario.ape2,
-                login: $scope.ajaxDatoUsuario.login,
-                id_tipoUsuario: $scope.ajaxDatoUsuario.obj_tipoUsuario.id
-            }
-            $http({
-                method: 'GET',
-                withCredentials: true,
-                url: 'json?ob=usuario&op=login&user=&pass=',
-                params: {json: JSON.stringify(json)}
-            }).then(function (response) {
-                $scope.status = response.status;
-                $scope.mensaje = true;
-            }, function (response) {
-                $scope.ajaxDataUsuario = response.data.message || 'Request failed';
-                $scope.status = response.status;
-            });
-        };
+            $scope.ob = "usuario";
+         $http({
+            method: 'GET',
+            url: 'json?ob='+$scope.ob+'&op=login&user='+$scope.login+'&pass='+$scope.password
+        }).then(function (response) {
+            $scope.status = response.status;
+            $scope.ajaxDataUsuarios = response.data.message;
 
+        }, function (response) {
+            $scope.status = response.status;
+            $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
+        });
+   
+        }
     }]);
